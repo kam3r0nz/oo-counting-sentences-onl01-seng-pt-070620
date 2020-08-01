1c535dc5c0c17d64985e4e90e7c06a01e916747e
Kameron Hungerford <kam.hungerford@gmail.com>
2020-08-01 21:32:10 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,22 +1,18 @@
-require 'pry'
-
 class String
-
   def sentence?
     self.end_with?(".")
   end
-
+  
   def question?
     self.end_with?("?")
   end
-
+  
   def exclamation?
     self.end_with?("!")
   end
-
+  
   def count_sentences
-    new_array = self.split(/[.?!]/)
-    new_array.delete_if{|x| x.empty?}
-    new_array.length
+    self.split(/[.!?]/).reject{|string| string.empty?}.length
   end
+  
 end
